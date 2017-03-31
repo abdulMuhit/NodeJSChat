9a029f7c547ef46b718dac6f0051ecbcf31f3d6b
nathansire <nathantech2005@gmail.com>
2017-03-31 13:41:55 -0400

    rollback to express 3.0.

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-var express = require('express'),
-  app = express()
+var app = express()
   , http = require('http')
   , server = http.createServer(app)
   , io = require('socket.io').listen(server);
